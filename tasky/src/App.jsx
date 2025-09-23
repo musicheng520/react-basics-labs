4a189addc3b672633639a2e6c3b97bcb6f8cdc39
Sicheng Mu <1958775255@qq.com>
2025-09-23 14:42:53 +0100

    Tasky Lab 2b - Exercises done

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -10,16 +10,17 @@ function App() {
 
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id:1,title:"Dishes", description: "Empty dishwasher", deadline: "Today", done:false },
-      { id:2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow",done:false },
-      { id:3, title: "Tidy up", deadline: "Today",done:false }
+      { id:1,title:"Dishes", description: "Empty dishwasher", deadline: "Today", done:false ,priority:"Medium"},
+      { id:2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow",done:false ,priority:"High"},
+      { id:3, title: "Tidy up", deadline: "Today",done:false ,priority:"Low"}
     ]
   });
 
     const [ formState, setFormState ] = useState({
     title: "",
     description: "",
-    deadline: ""
+    deadline: "",
+    priority: "Low"
   });
 
 
@@ -35,6 +36,9 @@ function App() {
           break;
       case "deadline":
           form.deadline = event.target.value;
+          break;
+      case "priority":
+          form.priority = event.target.value;
           break;
       default:
           form = formState;
@@ -84,6 +88,7 @@ return(
       description={task.description}
       deadline={task.deadline}
       key={task.id}
+      priority={task.priority}
       deleteTask = {() => deleteHandler(index)}
       markDone={() => doneHandler(index)}
       done={task.done}
